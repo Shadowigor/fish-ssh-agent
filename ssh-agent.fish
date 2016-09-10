83aa9db2d91e6989b94b9583c5a4982081fbaa56
Shadowigor <shadowigorr@gmail.com>
2016-09-10 21:01:49 +0200

    Initial commit

---
+++ ssh-agent.fish
@@ -0,0 +1,14 @@
+
+function ssha
+    set SSH_ENV_PATH /tmp/ssha-fish-env.tmp
+
+    if [ ! -e $SSH_ENV_PATH ]
+        ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV_PATH
+        . $SSH_ENV_PATH > /dev/null
+        ssh-add
+    else
+        . $SSH_ENV_PATH > /dev/null
+    end
+
+    exit 0
+end
